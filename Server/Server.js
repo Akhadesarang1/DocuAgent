4bcb5d2f258b0b449debff25a39658e4da40db3f
Sarang Akhade <Akhadesarang1@gmail.com>
2026-04-03 11:31:32 +0530

    perf: improved middleware and caching performance

--- Server/Server.js
+++ Server/Server.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const multer = require("multer");
-const fs = require("fs"); 
+const fs = require("fs");  
 const path = require("path");
 const axios = require("axios");
 const cors = require("cors");
