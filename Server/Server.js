2bcacc9cdaf4f8ae81b63f0ac9182175d8c613df
Sarang Akhade <Akhadesarang1@gmail.com>
2026-04-17 19:14:45 +0530

    chore : setup for upgradation of server

--- Server/Server.js
+++ Server/Server.js
@@ -2,7 +2,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose"); 
-const multer = require("multer");  
+const multer = require("multer");
 const fs = require("fs");
 const path = require("path");
 const axios = require("axios"); 
